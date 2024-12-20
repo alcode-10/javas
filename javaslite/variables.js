35f32d529db88f19627d82267efb4071877f0ae3
alcode-10 <114340017+alcode-10@users.noreply.github.com>
2024-12-20 17:28:43 +0000

    container configuration done

---
+++ javaslite/variables.js
@@ -0,0 +1,14 @@
+  const accountId=14456
+  let accountemail="manasvib@gmail.com"
+  var accountpassword="12345"
+  accountcity="bareilyy"
+  accountemail="abcde@google.com"
+  accountpassword="23456"
+  accountcity="lucknow"
+  let accountstate
+
+  /*prefer not to use var because of block and functional scope*/
+  //    accountId=2
+  console.log(accountId);
+  console.table([accountemail,accountpassword,accountcity,accountstate]);
+  
