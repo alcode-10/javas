01bd9578644f5bfe03b01efb79fe943bdf588aa2
alcode-10 <114340017+alcode-10@users.noreply.github.com>
2024-12-21 18:15:14 +0000

    container config done

---
+++ javaslite/datatypes.js
@@ -0,0 +1,15 @@
+ "use strict"//trealt all js code as newer version
+ //alert(5+5)//we are using node js,this works for browser,differnet syntax
+ console.log(3+3);
+ console.log("manasvi");
+ let name="manasvi"
+ let age=20
+ let status=true
+ //data types
+ //number
+ //bigint
+ //string=""
+ //boolean=true,false
+//null=standlone value;when value is there but empty,typeof=object
+//undefined=when variable value is not declared,typeof=undefined
+//symbol=unique
