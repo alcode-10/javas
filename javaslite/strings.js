0c14475f4d6a883bf1ea01feb8ce1bc58b1c25b9
alcode-10 <114340017+alcode-10@users.noreply.github.com>
2024-12-26 19:08:00 +0000

    strings done

---
+++ javaslite/strings.js
@@ -0,0 +1,24 @@
+const name="Manasvi"
+const repoCount=5
+//console.log(name+ +repoCount+" value")//normal method
+
+
+//String Interpolation(A model way to print a string using ${})
+//console.log(`Hello my name is ${name} and my repository count is ${repoCount}`);
+
+//otherway of declaring string is
+const gameName= new String('riyaaaa')
+
+//way to acess key value pair
+console.log(gameName[0]);
+//to acess prototype
+console.log(gameName.__proto__);
+//to use functions on string write variable.method
+//function to partition like
+//substring=only positive values,negative will not give error
+//slice(())=-ve values can work too
+//trim()-remove leading or trailing spaces
+//replace()-search and replace value
+//includes()-helps find a particular string
+//split()-helps convert string to an array by splitting it on basis of some seprator 
+//refer mdn web docs
