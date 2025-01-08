e2c68fefbdcf00007d82e4afa62ea3f628280e0a
alcode-10 <114340017+alcode-10@users.noreply.github.com>
2025-01-08 12:39:17 +0000

    control flow done

---
+++ javaslite3/01_controlflow1.js
@@ -0,0 +1,39 @@
+//if
+
+// if(condition)//condition should be true to execute the scope 
+// {
+
+// }
+// //operators for comparison= >,<,<=,>=,==,!=
+// //===,this checks for data type as well as value
+// if(condition)
+// {
+// }
+//     //statement 1;
+//  else
+//     {
+//         //statement 2;
+//     }
+//shorthand notations= -=,+=,/=,%=,*=
+//if we do not define scope we have to put semicolon at the end of statemnt
+//if we have to execute multiple statemnts without defining scope,we can put commas-not preffered
+if(true)
+console.log(`a`),
+console.log(`b`);
+
+// if else if ladder
+
+// if(condition1)
+// {}
+// else if(condition2)
+// {
+
+// }
+// else
+// {
+
+// }
+
+//to check multiple condition within single statement,use Logical Operators
+//they are=&&,||,!=
+
---
+++ javaslite3/02_controlflow.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+//works normally as in java,c and c++
+const choice=5
+switch(choice)
+{
+    case 1:
+    console.log(`one`)
+    break;
+    case 2:
+    console.log(`two`)
+    break;
+    case 3:
+    console.log(`three`)
+    break;
+    case 4:
+    console.log(`four`)
+    break;
+    default:
+    console.log(`Enter valid choice`);
+}
+//Fall through also works here wherein default is not executed
+//Case value can be integer or string
